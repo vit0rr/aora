bc488ad48f1389272b1ad8e08a826489dff1ffd6
vitor <vi.souza.almeida@protonmail.com>
2024-10-30 18:13:13 -0300

    feat(onboarding): remove unused import

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants";
 import CustomButton from "@/components/CustomButton";
 import { StatusBar } from "expo-status-bar";
-import { Redirect, router } from "expo-router";
+import { router } from "expo-router";
 
 export default function App() {
   return (
