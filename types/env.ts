967afed64e78b9ac398c02afcffe84d1480bf5ec
vitor <vi.souza.almeida@protonmail.com>
2024-11-12 09:37:53 -0300

    feat(auth): remove types

--- types/env.ts
+++
@@ -1,9 +0,0 @@
-export type Env = {
-  ENDPOINT: string;
-  PLATFORM: string;
-  PROJECT_ID: string;
-  DATABASE_ID: string;
-  USER_COLLECTION_ID: string;
-  VIDEO_COLLECTION_ID: string;
-  STORAGE_ID: string;
-};
